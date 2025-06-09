0e74351f833c926542be4b441b84ad7d942f1234
custombusinesssystems <80806333+custombusinesssystems@users.noreply.github.com>
2025-06-09 14:04:46 +1000

    first commit

---
+++ lib/signature_pad_rails.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "signature_pad_rails/version"
+
+module SignaturePadRails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/signature_pad_rails/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SignaturePadRails
+  VERSION = "0.1.0"
+end
